124883be41e83b592885237ec398c0e8a9060c0f
tonyishangu <tonyishangu@gmail.com>
2020-10-27 12:24:08 +0300

    contents

--- projects/urls.py
+++ projects/urls.py
@@ -4,13 +4,13 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns=[
-    url(r'^$',views.index, name='index'),
-    url(r'^profile/',views.profile, name='profile'),
-    url(r'^new_project/',views.submit_project, name='submit_project'),
-    url(r'^search/',views.search_project,name='search_project'),
-    url(r'^project/(\d+)',views.project,name='project'),
-    url(r'^api/profiles/$',views.ProfileList.as_view(),name='profile_list'),
-    url(r'^api/projects/$',views.ProjectList.as_view(),name='project_list'),
+    url('^$',views.index, name='index'),
+    url('^profile/',views.profile, name='profile'),
+    url('^new_project/',views.submit_project, name='submit_project'),
+    url('^search/',views.search_project,name='search_project'),
+    url('^project/(\d+)',views.project,name='project'),
+    url('^api/profiles/$',views.ProfileList.as_view(),name='profile_list'),
+    url('^api/projects/$',views.ProjectList.as_view(),name='project_list'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
