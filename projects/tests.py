6f6133231da8fa10311793a73c4a3e8d68a4523d
tonyishangu <tonyishangu@gmail.com>
2020-10-27 12:24:36 +0300

    contents

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,56 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Project,UserProfile
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        '''
+        Set Up method to run before each test case
+        '''
+        self.new_user = User.objects.create_user(username='user',password='user-password')
+        self.new_profile = UserProfile(id=1,user=self.new_user,profile_pic='photos/pic',bio='user-bio')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,UserProfile))
+
+
+    def test_save_profile(self):
+        self.new_profile.save_profile()
+        profiles = UserProfile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_delete_profile(self):
+            self.new_profile.delete_profile()
+            profiles = UserProfile.objects.all()
+            self.assertTrue(len(profiles) == 0)
+
+    def test_update_bio(self):
+        self.new_profile.save_profile()
+        self.new_profile = UserProfile.objects.get(id=1)
+        profile = self.new_profile
+        profile.update_bio('updated user-bio')
+        self.updated_profile = UserProfile.objects.get(id=1)
+        self.assertEqual(self.updated_profile.bio,'updated user-bio') 
+
+class ProjectTestClass(TestCase):    
+    def setUp(self):
+        self.new_user = User.objects.create_user(username='user',password='user-password')
+        self.new_profile = UserProfile(user=self.new_user)
+        self.new_profile.save()
+        self.new_project = Project(id=1,landing_page='photos/photo',project_title='Test Project',project_description='Test Description',user=self.new_user,live_site='http://livesite.com')   
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,UserProfile))
+
+    def test_save_project(self):
+        self.new_project.save_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) > 0)
+
+
+    def test_delete_project(self):
+        self.new_project.delete_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) == 0)
