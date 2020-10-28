ee1f7e5978c5f9502123617e7318fd1304f52e2b
tonyishangu <tonyishangu@gmail.com>
2020-10-28 12:04:43 +0300

    deploy

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -13,16 +13,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 import dj_database_url
-import cloudinary
 from decouple import config,Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-cloudinary.config(
-  cloud_name = config('CLOUD_NAME'),
-  api_key = config('CLOUD_API'),
-  api_secret = config('API_SECRET'),
-)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
