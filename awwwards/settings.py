52e0dc6026da97edcc0ecb3ce55459629929798b
tonyishangu <tonyishangu@gmail.com>
2020-10-28 08:46:45 +0300

    readme file

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -130,5 +130,5 @@ STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+#LOGIN_REDIRECT_URL = '/'
+#LOGOUT_REDIRECT_URL = '/'
