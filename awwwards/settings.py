ab58677cd7d3d244a0b7fe3c096915d06ee2ae4f
tonyishangu <tonyishangu@gmail.com>
2020-10-28 11:30:46 +0300

    deploy

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 import dj_database_url
+import cloudinary
 from decouple import config,Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
