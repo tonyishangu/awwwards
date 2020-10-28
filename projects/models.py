f4cfc733db7f0d584b824e3f5ffbc07b75a8ca1c
tonyishangu <tonyishangu@gmail.com>
2020-10-28 11:37:26 +0300

    deploy

--- projects/models.py
+++ projects/models.py
@@ -8,7 +8,7 @@ from cloudinary.models import CloudinaryField
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    profile_pic = CloudinaryField('images')
+    profile_pic = models.ImageField(upload_to='profile_photos',null=True,blank=True)
     bio = HTMLField()
     contact=models.CharField(max_length=12)
     linkedIn =  URLOrRelativeURLField()
