db36a97087f25be0c57ace90a823f775ed76edaf
tonyishangu <tonyishangu@gmail.com>
2020-10-28 11:55:25 +0300

    deploy

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -18,7 +18,11 @@ from decouple import config,Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+cloudinary.config(
+  cloud_name = config('CLOUD_NAME'),
+  api_key = config('CLOUD_API'),
+  api_secret = config('API_SECRET'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
--- projects/models.py
+++ projects/models.py
@@ -3,12 +3,11 @@ from url_or_relative_url_field.fields import URLOrRelativeURLField
 from django.contrib.auth.models import User
 from datetime import datetime
 from tinymce.models import HTMLField
-from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    profile_pic = models.ImageField(upload_to='profile_photos',blank=True)
+    profile_pic = models.ImageField(upload_to='profile_photos',null=True,blank=True)
     bio = HTMLField()
     contact=models.CharField(max_length=12)
     linkedIn =  URLOrRelativeURLField()
@@ -31,7 +30,7 @@ class UserProfile(models.Model):
 class Project(models.Model):
     project_title = models.CharField(max_length=40)
     project_description = HTMLField()
-    landing_page = CloudinaryField('images')
+    landing_page = models.ImageField(upload_to='landing_pages')
     live_site = URLOrRelativeURLField()
     user = models.ForeignKey(User,null=True,blank=True,on_delete=models.CASCADE)
     design = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
